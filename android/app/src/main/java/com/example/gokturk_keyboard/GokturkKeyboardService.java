da2ddce6445c62523348f59089314b3bf94ae1fd
oguzhan18 <oguzhancart1@gmail.com>
2024-07-20 02:10:16 +0300

    Problems solved.

--- android/app/src/main/java/com/example/gokturk_keyboard/GokturkKeyboardService.java
+++ android/app/src/main/java/com/example/gokturk_keyboard/GokturkKeyboardService.java
@@ -4,7 +4,6 @@ import android.inputmethodservice.InputMethodService;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class GokturkKeyboardService extends InputMethodService {
@@ -26,45 +25,44 @@ public class GokturkKeyboardService extends InputMethodService {
     }
 
     private void setupMainKeyboard(View keyboardView) {
-
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_1, "𐰀");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_2, "𐰁");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_3, "𐰂");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_4, "𐰃");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_5, "𐰄");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_6, "𐰅");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_7, "𐰆");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_8, "𐰇");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_9, "𐰈");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_10, "𐰉");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_11, "𐰊");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_12, "𐰋");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_13, "𐰌");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_14, "𐰍");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_15, "𐰎");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_16, "𐰏");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_17, "𐰐");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_18, "𐰑");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_19, "𐰒");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_20, "𐰓");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_21, "𐰔");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_22, "𐰕");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_23, "𐰖");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_24, "𐰗");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_25, "𐰘");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_26, "𐰙");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_27, "𐰚");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_28, "𐰛");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_29, "𐰜");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_30, "𐰝");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_31, "𐰞");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_32, "𐰟");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_33, "𐰠");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_34, "𐰡");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_35, "𐰢");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_36, "𐰣");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_37, "𐰤");
-        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_38, "𐰥");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_1, "𐰀", "A");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_2, "𐰁", "B");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_3, "𐰂", "C");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_4, "𐰃", "Ç");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_5, "𐰄", "D");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_6, "𐰅", "E");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_7, "𐰆", "F");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_8, "𐰇", "G");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_9, "𐰈", "Ğ");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_10, "𐰉", "H");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_11, "𐰊", "I");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_12, "𐰋", "İ");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_13, "𐰌", "J");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_14, "𐰍", "K");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_15, "𐰎", "L");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_16, "𐰏", "M");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_17, "𐰐", "N");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_18, "𐰑", "O");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_19, "𐰒", "Ö");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_20, "𐰓", "P");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_21, "𐰔", "R");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_22, "𐰕", "S");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_23, "𐰖", "Ş");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_24, "𐰗", "T");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_25, "𐰘", "U");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_26, "𐰙", "Ü");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_27, "𐰚", "V");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_28, "𐰛", "Y");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_29, "𐰜", "Z");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_30, "𐰝", "A");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_31, "𐰞", "B");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_32, "𐰟", "C");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_33, "𐰠", "Ç");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_34, "𐰡", "D");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_35, "𐰢", "E");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_36, "𐰣", "F");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_37, "𐰤", "G");
+        addLetterButtonClickListener(keyboardView, R.id.button_gokturk_letter_38, "𐰥", "Ğ");
 
         Button spaceButton = keyboardView.findViewById(R.id.button_space);
         if (spaceButton != null) {
@@ -88,8 +86,6 @@ public class GokturkKeyboardService extends InputMethodService {
     }
 
     private void setupNumbersKeyboard(View keyboardView) {
-        // Sayı ve özel karakter butonlarını ekleyin
-
         Button backButton = keyboardView.findViewById(R.id.button_back);
         if (backButton != null) {
             backButton.setOnClickListener(v -> setInputView(mainKeyboardView));
@@ -97,22 +93,22 @@ public class GokturkKeyboardService extends InputMethodService {
     }
 
     private void setupEmojiKeyboard(View keyboardView) {
-        // Emoji butonlarını ekleyin
-
         Button backButton = keyboardView.findViewById(R.id.button_back);
         if (backButton != null) {
             backButton.setOnClickListener(v -> setInputView(mainKeyboardView));
         }
     }
 
-    private void addLetterButtonClickListener(View keyboardView, int buttonId, String letter, String hint) {
+    private void addLetterButtonClickListener(View keyboardView, int buttonId, String letter, String equivalent) {
         Button button = keyboardView.findViewById(buttonId);
         if (button != null) {
             button.setOnClickListener(v -> getCurrentInputConnection().commitText(letter, 1));
             button.setOnLongClickListener(v -> {
-                Toast.makeText(this, hint, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), equivalent, Toast.LENGTH_SHORT).show();
                 return true;
             });
+        } else {
+            throw new IllegalArgumentException("Button not found with id: " + buttonId);
         }
     }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
